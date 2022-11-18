768b7f6ff6ccec4fd517cc69f004450bd1a0ed3a
michoball <myunghun0114@gmail.com>
2022-11-18 18:55:13 +0900

    chore: add antd library and making introduce section

--- src/App.js
+++ src/App.js
@@ -3,9 +3,48 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, world</h1>
-      </header>
+      <div
+        className="card-container"
+        style={{
+          border: "1px solid #fff",
+          width: "70vw",
+          height: "60vh",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <section
+          className="card-content"
+          style={{
+            width: "calc(70vw - 40%)",
+            border: "3px solid red",
+            padding: "10px 20px",
+          }}
+        >
+          <header className="card-header">
+            <h1>Hi, There ~ !</h1>
+          </header>
+          <article>
+            <p>프론트엔드 개발자 강명훈입니다.</p>
+            <p>
+              als;eijf;alsiejf;alsiejf;lasiejf;asliejf;laseijf;alseijf;lij
+              <br />
+              ;lasjef;iasje;lfijaselfijlij
+            </p>
+          </article>
+        </section>
+        <section
+          className="img-contianer"
+          style={{
+            color: "#fff",
+            fontSize: "50px",
+            width: "calc(70vw - 60%)",
+            border: "3px solid yellow",
+          }}
+        >
+          IMG
+        </section>
+      </div>
     </div>
   );
 }
