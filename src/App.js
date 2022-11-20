8ff6e934e1848b5396aaf58e7d477acd932242bc
michoball <myunghun0114@gmail.com>
2022-11-20 18:43:42 +0900

    chore: add more app styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import spaceComputer from "./assets/space-computer.jpg";
 
 function App() {
   return (
@@ -9,25 +10,15 @@ function App() {
             <h1>Hi, There ~ !</h1>
           </header>
           <article>
+            <h3>프론트엔드 개발자 강명훈입니다.</h3>
             <p>
-              프론트엔드 개발자 강명훈입니다. <br />
               als;eijf;alsiejf;alsiejf;lasiejf;asliejf;laseijf;alseijf;lij
-              <br />
               ;lasjef;iasje;lfijaselfijlij
             </p>
           </article>
         </section>
-        <section
-          className="img-contianer"
-          style={{
-            color: "#fff",
-            fontSize: "50px",
-            alignItems: "center",
-            width: "calc(70vw - 60%)",
-            border: "3px solid yellow",
-          }}
-        >
-          IMG
+        <section className="img-contianer">
+          <img src={spaceComputer} alt="spaceman-computer" />
         </section>
       </div>
     </div>
