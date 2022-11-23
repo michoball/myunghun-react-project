9669f654d1e1556fa101d544bb929e0b9f05a27f
michoball <myunghun0114@gmail.com>
2022-11-24 01:17:31 +0900

    chore: fix css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,28 @@
 import "./Contact.css";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
+  const navigate = useNavigate();
+  const backButtonHandler = () => {
+    navigate("/");
+  };
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
-      <div className="contact-info">
-        <p>
-          <span>Email</span>: myunghun0114@gmail.com
-        </p>
-        <p>
-          <span>P. H</span>: 010-6471-5307
-        </p>
-      </div>
+      <section className="contact-container">
+        <h1>Contact Me</h1>
+        <div className="contact-info">
+          <p>
+            <span>Email</span>: myunghun0114@gmail.com
+          </p>
+          <p>
+            <span>P. H</span>: 010-6471-5307
+          </p>
+        </div>
+        <button className="back-btn" onClick={backButtonHandler}>
+          <ArrowLeftOutlined />
+        </button>
+      </section>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,44 @@
 import "./Home.css";
 import { Button, Space } from "antd";
 import spaceComputer from "../assets/space-computer.jpg";
+import { useNavigate } from "react-router-dom";
+import CareerList from "../components/CareerList";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const contactButtonHandler = () => {
+    navigate("/contact");
+  };
   return (
-    <div className="card-container">
-      <section className="card-content">
-        <header className="card-header">
-          <h1>Hi, There ~ !</h1>
-        </header>
-        <article>
-          <h3>프론트엔드 개발자 강명훈입니다.</h3>
-          <p>
-            als;eijf;alsiejf;alsiejf;lasiejf;asliejf;laseijf;alseijf;lij
-            ;lasjef;iasje;lfijaselfijlij
-          </p>
-          <Space className="button-container">
-            <Button type="primary" size="large">
-              Contact Me
-            </Button>
-          </Space>
-        </article>
-      </section>
-      <section className="img-contianer">
-        <img src={spaceComputer} alt="spaceman-computer" />
-      </section>
+    <div className="home-container">
+      <div className="card-container">
+        <section className="card-content">
+          <header className="card-header">
+            <h1>Hi, There ~ !</h1>
+          </header>
+          <article>
+            <h3>프론트엔드 개발자 강명훈입니다.</h3>
+            <p>
+              als;eijf;alsiejf;alsiejf;lasiejf;asliejf;l
+              aseijf;alseijf;lij;lasjef;iasje;lfijaselfijlijasefasefasefasefasaffffffffffffffffffffffffffffffffffff
+            </p>
+            <Space className="button-container">
+              <Button
+                type="primary"
+                size="large"
+                onClick={contactButtonHandler}
+              >
+                Contact Me
+              </Button>
+            </Space>
+          </article>
+        </section>
+        <section className="img-contianer">
+          <img src={spaceComputer} alt="spaceman-computer" />
+        </section>
+      </div>
+
+      <CareerList />
     </div>
   );
 };
