670597eed4f06d638a9203bf223cc2fef9066d4b
michoball <myunghun0114@gmail.com>
2022-11-24 23:48:33 +0900

    feat: add change background image handler

--- src/App.js
+++ src/App.js
@@ -10,34 +10,41 @@ import { useEffect, useRef, useState } from "react";
 function App() {
   const [image, setImage] = useState(null);
   const imgRef = useRef();
-  const upsplash = createApi({
+  const unSplash = createApi({
     accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
   });
 
-  useEffect(() => {
-    upsplash.search
-      .getPhotos({ query: "space" })
-      .then((result) => {
-        setImage(result.response);
-      })
-      .catch(() => {
-        console.log("something went wrong!");
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const genRanNum = () => {
+    const ranNum = Math.floor(Math.random() * 9);
+    return ranNum;
+  };
+
+  const setBgImgHaneler = () => {
+    const num = genRanNum();
+    if (image)
+      imgRef.current.style.backgroundImage = `url(${image.results[num].urls.regular})`;
+  };
 
   useEffect(() => {
-    if (image) {
-      const ranNum = Math.floor(Math.random() * 9);
-      imgRef.current.style.backgroundImage = `url(${image.results[ranNum].urls.regular})`;
-    }
-  }, [image]);
+    const unsplashImg = async () => {
+      const result = await unSplash.search.getPhotos({ query: "space" });
+      setImage(result.response);
+      const num = genRanNum();
+      imgRef.current.style.backgroundImage = `url(${result.response.results[num].urls.regular})`;
+    };
+    unsplashImg();
+    setBgImgHaneler();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ConfigProvider theme={{ token: { colorPrimary: "#626C80" } }}>
       <div className="App" ref={imgRef}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home imgChangeHandler={setBgImgHaneler} />}
+          />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
