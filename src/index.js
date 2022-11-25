82e8db2e4b5c49dd9d468bd270893848560d7885
michoball <myunghun0114@gmail.com>
2022-11-25 22:57:09 +0900

    chore: add git deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_PUBLC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
